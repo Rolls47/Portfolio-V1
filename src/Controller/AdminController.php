5e8ae94a4849aa48326646d97865969bfaad5c4c
Rolls47 <quentin.adadain@gmail.com>
2020-07-22 23:48:52 +0200

    Admin Dashboard

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdminController
+ * @Route("/admin")
+ * @package App\Controller
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
