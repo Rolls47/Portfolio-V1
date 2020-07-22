5dfbd7b5cf2e7905279ad2ee35a8bb7eeeb3d1f7
Rolls47 <quentin.adadain@gmail.com>
2020-07-22 21:30:19 +0200

    PortFolio Add, edit, Delete OK

--- src/Form/PortfolioType.php
+++ src/Form/PortfolioType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Portfolio;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,26 @@ class PortfolioType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('technology')
-            ->add('image')
-            ->add('lien')
+            ->add('name', TextType::class, [
+                'label' => 'Nom :',
+                'label_attr' => ['class' => 'text-admin-form'],
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description :',
+                'label_attr' => ['class' => 'text-admin-form'],
+            ])
+            ->add('technology', TextType::class, [
+                'label' => 'Technologie :',
+                'label_attr' => ['class' => 'text-admin-form'],
+            ])
+            ->add('image', TextType::class, [
+                'label' => 'Image :',
+                'label_attr' => ['class' => 'text-admin-form'],
+            ])
+            ->add('lien', TextType::class, [
+                'label' => 'Lien :',
+                'label_attr' => ['class' => 'text-admin-form'],
+            ])
         ;
     }
 
